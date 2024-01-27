7db69b8d3fd456408b1a08baa7e7c636bb25dabd
Andrija Stojanović <stojanovicandrija123@gmail.com>
2024-01-28 00:13:28 +0100

    Added desserts data and images

---
+++ src/data/desserts.js
@@ -0,0 +1,134 @@
+let desserts = [
+    {
+        id: 1,
+        name: 'Kolac 1',
+        description: 'Opis 1',
+        ingredients: 'Sastojci 1',
+        picture: "Kolac_1.jpg",
+        price: 100,        
+        promotion: false,
+        isCake: false
+    },
+    {
+        id: 2,
+        name: 'Kolac 2',
+        description: 'Opis 2',
+        ingredients: 'Sastojci 2',
+        picture: "Kolac_2.jpg",        
+        price: 200,
+        promotion: true,
+        isCake: false
+    },
+    {
+        id: 3,
+        name: 'Kolac 3',
+        description: 'Opis 3',
+        ingredients: 'Sastojci 3',
+        picture: "Kolac_3.jpg",        
+        price: 300,
+        promotion: false,
+        isCake: false
+    },
+    {
+        id: 4,
+        name: 'Kolac 4',
+        description: 'Opis 4',
+        ingredients: 'Sastojci 4',
+        picture: "Kolac_4.jpg",        
+        price: 400,
+        promotion: true,
+        isCake: false
+    },
+    {
+        id: 5,
+        name: 'Kolac 5',
+        description: 'Opis 5',
+        ingredients: 'Sastojci 5',
+        picture: "Kolac_5.jpg",     
+        price: 500,   
+        promotion: false,
+        isCake: false
+    },
+    {
+        id: 6,
+        name: 'Kolac 6',
+        description: 'Opis 6',
+        ingredients: 'Sastojci 6',
+        picture: "Kolac_6.jpg",  
+        price: 600,      
+        promotion: true,
+        isCake: false
+    },
+    {
+        id: 7,
+        name: 'Kolac 7',
+        description: 'Opis 7',
+        ingredients: 'Sastojci 7',
+        picture: "Kolac_7.jpg",      
+        price: 700,  
+        promotion: false,
+        isCake: false
+    },
+    {
+        id: 8,
+        name: 'Torta 1',
+        description: 'Opis 1',
+        ingredients: 'Sastojci 1',
+        picture: "Torta_1.jpg",
+        price: 1000,        
+        promotion: false,
+        isCake: true
+    },
+    {
+        id: 9,
+        name: 'Torta 2',
+        description: 'Opis 2',
+        ingredients: 'Sastojci 2',
+        picture: "Torta_2.jpg",        
+        price: 2000,
+        promotion: true,
+        isCake: true
+    },
+    {
+        id: 10,
+        name: 'Torta 3',
+        description: 'Opis 3',
+        ingredients: 'Sastojci 3',
+        picture: "Torta_3.jpg",        
+        price: 3000,
+        promotion: false,
+        isCake: true
+    },
+    {
+        id: 11,
+        name: 'Torta 4',
+        description: 'Opis 4',
+        ingredients: 'Sastojci 4',
+        picture: "Torta_4.jpg",        
+        price: 4000,
+        promotion: true,
+        isCake: true
+    },
+    {
+        id: 12,
+        name: 'Torta 5',
+        description: 'Opis 5',
+        ingredients: 'Sastojci 5',
+        picture: "Torta_5.jpg",     
+        price: 5000,   
+        promotion: false,
+        isCake: true
+    },
+    {
+        id: 13,
+        name: 'Torta 6',
+        description: 'Opis 6',
+        ingredients: 'Sastojci 6',
+        picture: "Torta_6.jpg",  
+        price: 6000,      
+        promotion: true,
+        isCake: true
+    }
+]
+
+export default desserts
--- src/views/BuyerView.vue
+++ src/views/BuyerView.vue
@@ -3,7 +3,69 @@
 
     </NavigationHeader>
     <body>
-        
+        <div id ="promotions" class="row justify-content-center">
+            <div class="col-11 card">
+                <div class="row justify-content-evenly">
+                    <!-- <div class="col-4 card">
+                        aaaaaa
+                    </div> -->
+                    <!-- <div class="col-4 card">
+                        aaaaaa
+                    </div>
+                    <div class="col-4 card">
+                        aaaaaa
+                    </div> -->
+
+                    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="1000">
+                                <!-- <img src="../assets/logo.png" class="d-block w-100"> -->
+                            </div>
+                            <div class="carousel-item" data-bs-interval="2000">
+                                <!-- <img src="../assets/logout.png" class="d-block w-100"> -->
+                            </div>                            
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div id = "pastries" class="container">
+            <div class="row justify-content-between">
+                <div class="col-5 card">
+                    <h1 class="card-title ">Kolaci</h1>
+                    <div class="card-content">
+                        <div class="col-11">
+                            <!-- <div class="row" v-for="cake in cakes" :key="cake.id">
+                                {{ cake[0].name }}
+                            </div>  -->
+                            <div class="row">
+                                <img class="col-6" src="../../public/img/Torta_1.jpg">
+                                <div class="col-6">
+                                    <div class="row">{{ desserts[0].name }}</div>
+                                    <div class="row">{{ desserts[0].description }}</div>
+                                </div>
+                            </div>
+                            <div class="row justify-content-between">
+                                <img class="svg float-left" src="../assets/arrow-left-circle.svg">
+                                <img class="svg float-right" src="../assets/arrow-right-circle.svg">
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="col-5 card">
+                    <h1 class="card-title">Torte</h1>
+                </div>
+            </div>
+        </div>
     </body>
     <ContactInformation>
 
@@ -11,6 +73,8 @@
 </template>
 
 <script>
+
+    import desserts from '../data/desserts.js'
 
     import NavigationHeader from '../components/NavigationHeader.vue'
     import ContactInformation from '../components/ContactInformation.vue'
@@ -20,6 +84,30 @@
         components:{
             NavigationHeader,
             ContactInformation
+        },
+        data(){
+            return{                
+                allDesserts: [],
+                cakes: [],
+                promotions: [],
+                desserts: []
+            }
+        },
+        mounted(){
+            if(localStorage.getItem('desserts') != null){
+                this.allDesserts = JSON.parse(localStorage.getItem('desserts'))                                
+            }
+            else{ 
+                this.allDesserts = desserts                
+                // localStorage.setItem('desserts', JSON.stringify(this.allDesserts))
+            }
+
+            this.cakes = this.allDesserts.filter(dessert => dessert.isCake == true) 
+            console.log(this.cakes)                       
+            this.desserts = this.allDesserts.filter(dessert => dessert.isCake == false)            
+            console.log(this.desserts)                       
+            this.promotions = this.allDesserts.filter(dessert => dessert.promotion == true)            
+            console.log(this.promotions)                       
         }
     }
 
@@ -30,6 +118,24 @@
   body{
     background-image: url('../assets/background.jpg');
     background-size: cover;    
+  }
+
+  #promotions{
+    position: absolute;
+    width: 100%;
+    height: 35%;    
+    top: 10%;
+  }
+
+  #pastries{
+    position: absolute;    
+    height: 40%;    
+    left: 15%;
+    top: 50%;
+  }
+
+  .svg{
+    width: 10%;
   }
   
 </style>
